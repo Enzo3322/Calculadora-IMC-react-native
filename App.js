a01536cdb37a7c9419c4cc9cc8440e14632a6ad1
Enzo Spagnolli <enzo.spag14@gmail.com>
2020-09-16 20:49:26 -0300

    firstcommit

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handlerSubmit(){
+    const alt = altura /100;
+    const imc = peso / (alt*alt);
+    alert(imc);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={styles.title}>Calcule seu IMC</Text>
+
+    <TextInput
+    style={styles.input}
+    value={peso}
+    onChangeText={(peso) => setPeso (peso)}
+    placeholder="Peso (kg)"
+    keyboardType="numeric"
+    />
+  
+    <TextInput
+    style={styles.input}   
+    value={altura}
+    onChangeText={(altura) => setAltura (altura)}
+    placeholder="Altura (cm)"
+    keyboardType="numeric"
+    />
+
+    <TouchableOpacity 
+    style={styles.button}
+    onPress={handlerSubmit}
+    >
+      <Text style={styles.buttonText}>Calcular</Text>
+    </TouchableOpacity>
+
     </View>
   );
 }
@@ -14,8 +46,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title:{
+    fontSize:30,
+    marginTop:25,
+    textAlign:'center'
+  },
+  input:{
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin:15,
+    padding:10,
+    color: '#FFF',
+    fontSize:23,
+  },
+  button:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor:'#41Aef4',
+    padding: 10,
+  },
+  buttonText:{
+    color:'#FFF',
+    fontSize:25,
+  }
 });
